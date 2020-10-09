b006026c9a8ad8963006e446c18f4cf6639cfadf
monoue <like_a_newborn_baby@icloud.com>
2020-10-09 11:58:59 +0900

    42 Docs started

--- minilibx/main.c
+++ minilibx/main.c
@@ -1,70 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../mlx/mlx.h"
+#include "mlx/mlx.h"
 
-#define X_EVENT_KEY_PRESS		2
-#define X_EVENT_KEY_release		3
-#define X_EVENT_KEY_EXIT		17 //exit key code
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
-//Mac key code example
-//All the key code example other than below is described on the site linked in READEME.md
-#define KEY_ESC			53
-# define KEY_Q			12
-# define KEY_W			13
-# define KEY_E			14
-# define KEY_R			15
-# define KEY_A			0
-# define KEY_S			1
-# define KEY_D			2
-
-//Since key_press() can recieve only one argument, all the argument shold be gathered in one structure
-//x,y and str are meaningless variables.
-typedef struct s_param{
-	int		x;
-	int		y;
-	char	str[3];
-}				t_param;
-
-//Only param->x will be used.
-void			param_init(t_param *param)
+void			my_mix_pixel_put(t_data *data, int x, int y, int color)
 {
-	param->x = 3;
-	param->y = 4;
-	param->str[0] = 'a';
-	param->str[1] = 'b';
-	param->str[2] = '\0';
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned *)dst = color;
 }
 
-int				key_press(int keycode, t_param *param)
+int	main()
 {
-	static int a = 0;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
-	if (keycode == KEY_W)//Action when W key pressed
-		param->x++;
-	else if (keycode == KEY_S) //Action when S key pressed
-		param->x--;
-	else if (keycode == KEY_ESC) //Quit the program when ESC key pressed
-		exit(0);
-	printf("x: %d\n", param->x);
-	return (0);
-}
-
-int			main(void)
-{
-	void		*mlx;
-	void		*win;
-	t_param		param;
-
-	param_init(&param);
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "mlx_project");
-	printf("-------------------------------\n");
-	printf("'W key': Add 1 to x.\n");
-	printf("'A key': Subtract 1 from x\n");
-	printf("'ESC key': Exit this program\n");
-	printf("'Other keys': print current x \n");
-	printf("-------------------------------\n");
-	printf("Current x = 3\n");
-	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hellow world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FFFFFF);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
