f442e1769ab5a000707419003e16e6096dd0c6cb
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2020-11-18 16:54:50 +0900

    saving bitmap started

---
+++ src/save_bitmap.h
@@ -0,0 +1,28 @@
+#ifndef SAVE_BITMAP_H
+# define SAVE_BITMAP_H
+
+#define FILE_HEADER_SIZE 14
+#define INFO_HEADER_SIZE 40
+#define HEADER_SIZE (FILE_HEADER_SIZE + INFO_HEADER_SIZE)
+
+typedef struct	s_rgb{
+	unsigned char b;
+	unsigned char g;
+	unsigned char r;
+}				t_rgb;
+
+typedef struct	s_image{
+	unsigned int	height;
+	unsigned int	width;
+	t_rgb			*data;
+}				t_image;
+
+t_image *Read_Bmp(char *filename);
+
+int Write_Bmp(char *filename, t_image *img);
+
+t_image *Create_Image(int width, int height);
+void Free_Image(t_image *img);
+
+
+#endif
