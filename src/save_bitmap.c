d365c03eac8c26e86c27e4c8a66732e069300033
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2020-11-19 17:03:54 +0900

    bmp done, though needs error handling and refactoring

--- src/save_bitmap.c
+++ src/save_bitmap.c
@@ -6,99 +6,65 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 09:19:48 by monoue            #+#    #+#             */
-/*   Updated: 2020/11/19 15:08:01 by monoue           ###   ########.fr       */
+/*   Updated: 2020/11/19 17:00:55 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "save_bitmap.h"
 
-void	ft_bdata(int fd)
+int		calc_current_location_in_image_buf(int x, int y, const int image_buf_size)
 {
-	const int *addr = (const int *)g_img.addr;
+	const int line_size = g_cubfile_data.window_width * 4;
 
-	int				i;
-	int				j;
-	unsigned char	buffer[4];
+	return (image_buf_size - (line_size * (y + 1)) + x * 4);
+}
 
-	i = g_cubfile_data.window_width * (g_cubfile_data.window_height - 1);
-	while (i >= 0)
+void	set_color_element(unsigned char *image_buf, const int current_location_in_buf, const int pixel_location, size_t color_element_index)
+{
+	const int	*addr = (const int *)g_img.addr;
+	
+	image_buf[current_location_in_buf + color_element_index] = (addr[pixel_location] >> (color_element_index * 8)) % (2 << 8);
+}
+
+void	set_current_pixel(unsigned char *image_buf, const int image_buf_size, int x, int y)
+{
+	const int	current_location_in_buf = calc_current_location_in_image_buf(x, y, image_buf_size);
+	const int	pixel_location = g_cubfile_data.window_width * y + x;
+	size_t		color_element_index;
+
+	color_element_index = 0;
+	while (color_element_index < 4)
 	{
-		j = 0;
-		while (j < g_cubfile_data.window_width)
-		{
-			buffer[0] = (unsigned char)(addr[i] % 256);
-			buffer[1] = (unsigned char)(addr[i] / 256 % 256);
-			buffer[2] = (unsigned char)(addr[i] / 256 / 256 % 256);
-			buffer[3] = (unsigned char)(addr[i] / 256 / 256 / 256);
-			// buffer[3] = (unsigned char)(0);
-			write(fd, buffer, 4);
-			i++;
-			j++;
-		}
-		i -= 2 * g_cubfile_data.window_width;
+		set_color_element(image_buf, current_location_in_buf, pixel_location, color_element_index);
+		color_element_index++;
 	}
 }
 
-// void	ft_bdata(int fd)
-// {
-// 	const int *addr = (const int *)g_img.addr;
-
-// 	int				pixel_location;
-// 	int				x;
-// 	const int 
-// 	unsigned char	image_buf[g_cubfile_data.window_height * g_cubfile_data.window_width * 4];
-
-// 	pixel_location = g_cubfile_data.window_width * (g_cubfile_data.window_height - 1);
-// 	while (pixel_location >= 0)
-// 	{
-// 		x = 0;
-// 		while (x < g_cubfile_data.window_width)
-// 		{
-// 			image_buf[0] = (unsigned char)(addr[pixel_location] % 256);
-// 			image_buf[1] = (unsigned char)(addr[pixel_location] / 256 % 256);
-// 			image_buf[2] = (unsigned char)(addr[pixel_location] / 256 / 256 % 256);
-// 			image_buf[3] = (unsigned char)(0);
-// 			write(fd, image_buf, 4);
-// 			pixel_location++;
-// 			x++;
-// 		}
-// 		pixel_location -= 2 * g_cubfile_data.window_width;
-// 	}
-
-// 	int y = g_cubfile_data.window_height - 1;
-
-// 	while (y >= 0)
-// 	{
-// 		x = 0;
-// 		while (x < g_cubfile_data.window_width)
-// 		{
-// 			pixel_location = g_cubfile_data.window_width * y + x;
-// 			// TODO: ビットシフトで書き直す
-// 			image_buf[] = addr[pixel_location] % 256;
-// 		}
-// 	}
-// 	// while (pixel_location >= 0)
-// 	{
-// 		x = 0;
-// 		while (x < g_cubfile_data.window_width)
-// 		{
-// 			image_buf[0] = (unsigned char)(addr[pixel_location] % 256);
-// 			image_buf[1] = (unsigned char)(addr[pixel_location] / 256 % 256);
-// 			image_buf[2] = (unsigned char)(addr[pixel_location] / 256 / 256 % 256);
-// 			image_buf[3] = (unsigned char)(0);
-// 			write(fd, image_buf, 4);
-// 			pixel_location++;
-// 			x++;
-// 		}
-// 		pixel_location -= 2 * g_cubfile_data.window_width;
-// 	}
-// }
-
-static void	set_file_header_data(unsigned char header_buf[HEADER_SIZE])
+void	write_image(int fd)
 {
-	// const int			real_width = g_cubfile_data.window_width * 3 + g_cubfile_data.window_width % 4;
-	const int			real_width = g_cubfile_data.window_width * 4;
-	const unsigned int	file_size = g_cubfile_data.window_height * real_width + HEADER_SIZE;
+	int				y;
+	int				x;
+	const int 		image_buf_size = g_cubfile_data.window_height * g_cubfile_data.window_width * 4 ;
+	unsigned char	image_buf[image_buf_size];
+
+	ft_bzero(image_buf, sizeof(image_buf));
+	y = 0;
+	while (y < g_cubfile_data.window_height)
+	{
+		x = 0;
+		while (x < g_cubfile_data.window_width)
+		{
+			set_current_pixel(image_buf, image_buf_size, x, y);
+			x++;
+		}
+		y++;
+	}
+	write(fd, image_buf, image_buf_size);
+}
+
+static void	set_file_header(unsigned char header_buf[HEADER_SIZE], const int line_size)
+{
+	const unsigned int	file_size = g_cubfile_data.window_height * line_size + HEADER_SIZE;
 
 	header_buf[FILE_TYPE_1_OFFSET] = 'B';
 	header_buf[FILE_TYPE_2_OFFSET] = 'M';
@@ -108,18 +74,15 @@ static void	set_file_header_data(unsigned char header_buf[HEADER_SIZE])
 	header_buf[OFFSET_TO_PIXEL_DATA_OFFSET] = HEADER_SIZE;
 }
 
-static void	set_info_header_data(unsigned char header_buf[HEADER_SIZE])
+static void	set_info_header(unsigned char header_buf[HEADER_SIZE], const int line_size)
 {
-	// const int			real_width = g_cubfile_data.window_width * 3 + g_cubfile_data.window_width % 4;
-	const int			real_width = g_cubfile_data.window_width * 4;
-	const unsigned int	image_size = g_cubfile_data.window_height * real_width;
+	const unsigned int	image_size = g_cubfile_data.window_height * line_size;
 
 	header_buf[INFO_HEADER_SIZE_OFFSET] = INFO_HEADER_SIZE;
 	ft_memcpy(&header_buf[IMAGE_WIDTH_OFFSET], &g_cubfile_data.window_width, sizeof(g_cubfile_data.window_width));
 	ft_memcpy(&header_buf[IMAGE_HEIGHT_OFFSET], &g_cubfile_data.window_height, sizeof(g_cubfile_data.window_height));
 	header_buf[PLANES_OFFSET] = PLANES;
-	// header_buf[BITS_PER_PIXEL_OFFSET] = BITS_PER_PIXEL;
-	header_buf[BITS_PER_PIXEL_OFFSET] = 32;
+	header_buf[BITS_PER_PIXEL_OFFSET] = g_img.bits_per_pixel;
 	ft_memset(&header_buf[COMPRESSION_METHOD_OFFSET], 0, 4);
 	ft_memcpy(&header_buf[IMAGE_SIZE_OFFSET], &image_size, sizeof(image_size));
 	ft_memset(&header_buf[HORZ_RESOLUTION_OFFSET], 0, 4);
@@ -128,34 +91,30 @@ static void	set_info_header_data(unsigned char header_buf[HEADER_SIZE])
 	ft_memset(&header_buf[IMPORTANT_COLORS_OFFSET], 0, 4);
 }
 
-int		take_screenshot()
+void	write_header(int fd)
 {
-	int				fd;
-	unsigned char	header_buf[HEADER_SIZE];
-	
+	unsigned char		header_buf[HEADER_SIZE];
+	const int			line_size = g_cubfile_data.window_width * 4;
+
 	ft_bzero(header_buf, sizeof(header_buf));
-	fd = open("bitmap.bmp", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-	set_file_header_data(header_buf);
-	set_info_header_data(header_buf);
+	set_file_header(header_buf, line_size);
+	set_info_header(header_buf, line_size);
 	write(fd, header_buf, HEADER_SIZE);
+}
 
+void	write_bmp_file()
+{
+	int	fd;
 
-	// draw_wolf(e);
-
-	// write_file_header(fd);
-	// write_info_header(fd);
-	ft_bdata(fd);
+	fd = open("bitmap.bmp", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+	write_header(fd);
+	write_image(fd);
 	close(fd);
-	// free(e->mlx);
-	// free(e->edit);
-	return (1);
 }
 
 void	make_screenshot()
 {
-	if (!take_screenshot())
-		ft_putstr("hogehoge");
-		// put_error("ERROR\nCouldn't create/open screenshot.bmp\n");
+	write_bmp_file();
 	write(1, "Screenshot saved!\n", 18);
 	exit(1);
 }
